57f35c21fce4bba339333208bdf4481d2a500e37
savperera369 <saviru2002@outlook.com>
2024-09-22 17:18:24 -0400

    initial stripe setup

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { DrizzleChat } from '@/lib/db/schema';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from './ui/button';
 import { MessageCircle, PlusCircle } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import axios from 'axios';
 
 type Props = {
     chats: DrizzleChat[],
@@ -12,6 +13,21 @@ type Props = {
 };
 
 const ChatSideBar = ({ chats, chatId }: Props) => {
+    const [loading, setLoading] = useState(false);
+
+    const handleSubscription = async () => {
+        try {
+            setLoading(true);
+            // response will contain a url
+            const response = await axios.get('/api/stripe');
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log('error', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className="w-full h-screen p-4 text-gray-200 bg-gray-900">
             <Link href="/">
@@ -41,6 +57,9 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
                     <Link href="/">Source</Link>
                     {/* Stripe Button */}
                 </div>
+                <Button className='mt-2 text-white bg-slate-700' disabled={loading} onClick={handleSubscription}>
+                    Upgrade to Pro!
+                </Button>
             </div>
         </div>
     );
